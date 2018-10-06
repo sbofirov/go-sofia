c8ff73a5cccdd5c36900ecbdaace36408094fb9a
Stefan Bofirov <stefan.bofirov@dnhsoft.com>
2018-10-06 12:07:52 +0300

    Add env's for server port

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -4,25 +4,36 @@ import (
   "log"
   "net/http"
   "fmt"
+  "os"
 
   "github.com/gorilla/mux"
   "github.com/sbofirov/go-sofia/internal/diagnostics"
 )
 
 func main() {
-    log.Print("Hello world!")
+    log.Print("Starting the application...")
+
+    blPort := os.Getenv("PORT")
+    if (len(blPort) == 0) {
+      log.Fatal("BL port must be set!")
+    }
+
+    diagnosticsPort := os.Getenv("DIAG_PORT")
+    if (len(diagnosticsPort) == 0) {
+      log.Fatal("Diagnostics port must be set!")
+    }
 
     router := mux.NewRouter()
     router.HandleFunc("/", handleRequest)
     go func () {
-      err := http.ListenAndServe(":8080", router)
+      err := http.ListenAndServe(":"+blPort, router)
       if err != nil {
         log.Fatal(err)
       }
     }()
 
     diagnostics := diagnostics.NewDiagnostics()
-    err := http.ListenAndServe(":8585", diagnostics)
+    err := http.ListenAndServe(":"+diagnosticsPort, diagnostics)
     if err != nil {
       log.Fatal(err)
     }
